540988a96b1f40f4658d6c4675ab53c1109df44c
lawback/lipeifeng <610466535qq.com>
2020-08-31 09:54:38 +0800

    更新本地操作

--- server/api/userApi.js
+++ server/api/userApi.js
@@ -7,30 +7,57 @@ var $sql = require('../sqlMap');
 var conn = mysql.createConnection(models.mysql);
 
 conn.connect();
+// 操作成功
 var jsonWrite = function(res, ret) {
+    console.log(ret)
     if(typeof ret === 'undefined') {
         res.json({
             code: '1',
             msg: '操作失败'
         });
     } else {
-        res.json(ret);
+        res.json({
+            code: '0',
+            msg: '操作成功'
+        });
     }
 };
 
 // 增加用户接口
 router.post('/addUser', (req, res) => {
     var sql = $sql.user.add;
+    var table = $sql.user.get
     var params = req.body;
-    console.log(params);
-    conn.query(sql, [params.name, params.age], function(err, result) {
+    var index = 0
+    var list = []
+    conn.query(table,function(err,result){
         if (err) {
             console.log(err);
         }
         if (result) {
-            jsonWrite(res, result);
+        
+         list = result
+         console.log(list)
+         for(var i=0;i<list.length;i++) {
+             console.log(i)
+            console.log(list[i].id)
+            if(list[i].id>=index){
+                index = list[i].id+1
+            }
+        }
+        conn.query(sql, [index,params.name, params.age], function(err, result) {
+            if (err) {
+                console.log(err);
+            }
+            if (result) {
+                jsonWrite(res, result);
+            }
+        })
         }
     })
+    console.log('index')
+    console.log(index)
+   
 });
 
 module.exports = router;
--- server/sqlMap.js
+++ server/sqlMap.js
@@ -1,7 +1,8 @@
 var sqlMap = {
     // 用户
     user: {
-        add: 'INSERT INTO user(id, name, age) values (1, ?, ?)'
+        add: 'INSERT INTO user(id, name, age) values (?, ?, ?)',
+        get:'SELECT * FROM user'
     }
 }
 
